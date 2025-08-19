cc8984be2579d73c73eba5c73b75487c61c7eea3
Noookia <matchman7980@gmail.com>
2025-08-20 01:03:13 +0800

    Modify graph init-list

--- fpf/graph.cpp
+++ fpf/graph.cpp
@@ -2,8 +2,8 @@
 
 namespace fpf {
 // Constructs a w1 by w2 non-dynamic bit matrix filled with 0
-BitMatGraph2D::BitMatGraph2D(size_t N) : matrix(N, false), edge(N * N, 0) {
-  size = N;
+BitMatGraph2D::BitMatGraph2D(size_t N)
+    : matrix(N, false), size(N), edge(N * N, 0) {
   for (size_t i = 0; i < N; i++) {
     [[maybe_unused]] auto row = matrix.construct_row(i);
   }
