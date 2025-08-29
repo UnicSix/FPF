824726dca3d53902a04c4772ea4bd2768edf451f
Noookia <matchman7980@gmail.com>
2025-08-29 22:31:29 +0800

    Adjust bellman-ford vertex travesal

--- fpf/bellman-ford.hpp
+++ fpf/bellman-ford.hpp
@@ -17,8 +17,9 @@ std::vector<T> FindPath(const BitMatGraph2D<T>& graph, size_t src) {
   std::vector<T> dists(graph.Size(), INF);
   dists[src] = 0;
 
-  for (size_t cur = 0; cur < graph.Size(); ++cur) {
-    auto cur_row = graph.connection_.get_row(cur);
+  for (size_t i = 0; i < graph.Size(); ++i) {
+    size_t cur     = (src + i) % graph.Size();
+    auto   cur_row = graph.connection_.get_row(cur);
     for (auto vtx = cur_row->first(); vtx != cur_row->end(); ++vtx) {
       if (dists[vtx.value()] > dists[cur] + graph.DistAt(cur, vtx)) {
         dists[vtx.value()] = dists[cur] + graph.DistAt(cur, vtx);
